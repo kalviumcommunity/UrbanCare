d7977022c50819fe89e4de0ddd5b82738b5e1d01
Vijayashree A <avijayashree06@gmail.com>
2026-01-28 14:09:59 +0530

    HTTP-Enforment

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,16 +1,16 @@
-import styles from "./home.module.css";
-import Link from "next/link";
+// import styles from "./ThemeToggle";
+// import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className={styles.hero}>
-      <h1>Smart City Complaint Portal</h1>
-      <p>Report issues. Track progress. Improve your city.</p>
+// export default function Home() {
+//   return (
+//     // <div className={styles.hero}>
+//     //   <h1>Smart City Complaint Portal</h1>
+//     //   <p>Report issues. Track progress. Improve your city.</p>
 
-      <div className={styles.actions}>
-        <Link href="/auth/signup" className={styles.btnPrimary}>Get Started</Link>
-        <Link href="/auth/login" className={styles.btnSecondary}>Login</Link>
-      </div>
-    </div>
-  );
-}
+//     //   <div className={styles.actions}>
+//     //     <Link href="/auth/signup" className={styles.btnPrimary}>Get Started</Link>
+//     //     <Link href="/auth/login" className={styles.btnSecondary}>Login</Link>
+//     //   </div>
+//     // </div>
+//   );
+// }
